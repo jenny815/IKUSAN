de6a3462decb36fbc744608a20cae4eca6f2f8f0
jenny815 <jennylike815@gmail.com>
2022-05-08 15:22:55 +0800

    上傳檔案

---
+++ back/index.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose'
+import express from 'express'
+import dotenv from 'dotenv'
+import bodyParser from 'body-parser'
+
+dotenv.config()
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.listen(process.env.Port, () => {
+  console.log('server start')
+})
